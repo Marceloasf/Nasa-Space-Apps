2d66a4c4118c8e2d2cfcb11ae37a58eaf5cc945e
unknown <thiagobussola@hotmail.com>
2019-10-20 03:31:04 -0300

    Front-end

---
+++ js/LayersMaps/VegetationIndex.js
@@ -0,0 +1,28 @@
+var vegetationIndex = document.querySelector("#VegetationIndex");
+
+vegetationIndex.addEventListener("click", function () {
+    console.log("Chamando Vegetation");
+
+    buildGlobe();
+
+    var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
+    
+    var layerClima = "MOD_NDVI_M";
+
+    var createLayer = function (xmlDom) {
+        var wms = new WorldWind.WmsCapabilities(xmlDom);
+        var wmsLayerCapabilities = wms.getNamedLayer(layerClima);
+        var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
+        var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+        wwd.addLayer(wmsLayer);
+    };
+
+    var logError = function (jqXhr, text, exception) {
+        console.log("There was a failure retrieving the capabilities document: " +
+            text +
+            " exception: " + exception);
+    };
+
+    $.get(serviceAddress).done(createLayer).fail(logError);
+
+})
